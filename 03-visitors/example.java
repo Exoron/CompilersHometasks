1cab1b2a0eb3cc6aa56daffebc68b9f5fc3da9d6
Exoron <twexoron@gmail.com>
2020-05-24 18:59:15 +0300

    Actually changed examples

--- 03-visitors/example.java
+++ 03-visitors/example.java
@@ -1,8 +1,10 @@
 int a;
 a = 10;
 int b = 4;
-System.out.println(42);
-System.out.println(a);
-System.out.println(b);
-System.out.println(a+b);
-System.out.println(a/b);
+{
+    System.out.println(42);
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(a+b);
+    System.out.println(a/b);
+}
