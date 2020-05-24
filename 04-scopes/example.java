a1fd615d7f4a144d47e198c7fa2e4c323b5d25ad
Exoron <twexoron@gmail.com>
2020-05-25 02:50:25 +0300

    Improved the code example

--- 04-scopes/example.java
+++ 04-scopes/example.java
@@ -1,12 +1,23 @@
 int a;
 a = 10;
 int b = 4;
+
+System.out.println(42);
+System.out.println(a);
+System.out.println(b);
+System.out.println(a+b);
+System.out.println(a/b);
+
 {
-    System.out.println(42);
+    int a = 20;
+    b = 22;
     System.out.println(a);
     System.out.println(b);
     System.out.println(a+b);
-    System.out.println(a/b);
 }
-{}
-{}
+
+{
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(a+b);
+}
