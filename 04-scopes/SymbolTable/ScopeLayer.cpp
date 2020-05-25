2fd2dc2d02946ec437143eb6f213a806220c742d
Exoron <twexoron@gmail.com>
2020-05-25 03:13:02 +0300

    Added more code examples and fixed error messages

--- 04-scopes/SymbolTable/ScopeLayer.cpp
+++ 04-scopes/SymbolTable/ScopeLayer.cpp
@@ -5,24 +5,25 @@
 
 #include <iostream>
 
-ScopeLayer::ScopeLayer(ScopeLayer* parent): parent_(parent) {
+ScopeLayer::ScopeLayer(ScopeLayer* parent) : parent_(parent) {
   parent_->AddChild(this);
 }
 
-ScopeLayer::ScopeLayer(): parent_(nullptr) {}
+ScopeLayer::ScopeLayer() : parent_(nullptr) {}
 
 void ScopeLayer::DeclareVariable(Symbol symbol) {
   if (values_.find(symbol) != values_.end()) {
-    throw std::runtime_error("Variable has declared");
+    throw std::runtime_error("Variable `" + symbol.GetName() +
+                             "` has already been declared in "
+                             "this scope");
   }
 
   values_[symbol] = std::make_shared<UninitObject>();
-  //offsets_[symbol] = symbols_.size();
+  // offsets_[symbol] = symbols_.size();
   symbols_.push_back(symbol);
 }
 
 void ScopeLayer::Put(Symbol symbol, std::shared_ptr<Object> value) {
-
   ScopeLayer* current_layer = this;
 
   while (!current_layer->Has(symbol) && current_layer->parent_ != nullptr) {
@@ -31,14 +32,14 @@ void ScopeLayer::Put(Symbol symbol, std::shared_ptr<Object> value) {
   if (current_layer->Has(symbol)) {
     current_layer->values_.find(symbol)->second = value;
   } else {
-    throw std::runtime_error("Variable not declared");
+    throw std::runtime_error("Variable `" + symbol.GetName() +
+                             "` is not declared");
   }
 }
 
 bool ScopeLayer::Has(Symbol symbol) {
   return values_.find(symbol) != values_.end();
 }
-
 
 std::shared_ptr<Object> ScopeLayer::Get(Symbol symbol) {
   ScopeLayer* current_layer = this;
@@ -53,14 +54,8 @@ std::shared_ptr<Object> ScopeLayer::Get(Symbol symbol) {
   }
 }
 
-ScopeLayer* ScopeLayer::GetChild(size_t index) {
-  return children_[index];
-}
+ScopeLayer* ScopeLayer::GetChild(size_t index) { return children_[index]; }
 
-void ScopeLayer::AddChild(ScopeLayer* child) {
-  children_.push_back(child);
-}
+void ScopeLayer::AddChild(ScopeLayer* child) { children_.push_back(child); }
 
-ScopeLayer* ScopeLayer::GetParent() const {
-  return parent_;
-}
+ScopeLayer* ScopeLayer::GetParent() const { return parent_; }
---
+++ 04-scopes/example2.java
@@ -0,0 +1,4 @@
+int a = 42;
+System.out.println(a);
+b = 10;
+System.out.println(b);
